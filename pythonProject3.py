de1517c10a7f7f12b6bcde6db175f1e9d7f8e27c
exodise0404 <exodise0404@gmail.com>
2024-07-13 08:48:09 +0800

    Add files via upload

---
+++ pythonProject3.py
@@ -0,0 +1,5 @@
+name_one=12
+name_second=1.5
+name_three="Python"
+name_four="name_second/name_one"
+print('Курс:',name_three,','' всего задач:',name_one,', затрачено часов:',name_second,', среднее время выполнения',name_second/name_one,'часа')
